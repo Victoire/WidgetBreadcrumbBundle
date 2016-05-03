9786ae014d73584d1ffb32c8ee210c1da4990595
Charlie <charlielucas.cl@gmail.com>
2016-05-03 11:17:15 +0200

    use view and parents views name to set the cache

--- Entity/WidgetBreadcrumb.php
+++ Entity/WidgetBreadcrumb.php
@@ -13,4 +13,25 @@ use Victoire\Bundle\WidgetBundle\Entity\Widget;
  */
 class WidgetBreadcrumb extends Widget
 {
+    /**
+     * Generate the cache ID with the path build with parents view name
+     * to invalid when a parent name is modified.
+     *
+     * @return string
+     */
+    public function generateCacheId()
+    {
+        $view = $this->getCurrentView();
+        $nameChain = $view->getReference()->getName();
+        while ($view = $view->getParent()) {
+            $nameChain .= '/'.$view->getReference()->getName();
+        }
+
+        return sprintf('%s-%s-%s-%s',
+            $this->getId(),
+            $this->getUpdatedAt()->getTimestamp(),
+            md5($nameChain),
+            $this->getCurrentView()->getReference()->getId()
+        );
+    }
 }
